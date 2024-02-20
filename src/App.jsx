2be2fcfdd74051a34b05a62b483238a1668e9473
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-02-20 16:52:06 +0500

    code update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
+import gptLogo from "./Assets/chatgpt.svg";
 
 const App = () => {
   return (
-    <div className='App'>
+    <div className="App">
       <div className="sidebar">
-        
+        <div className="upperSide">
+          <div className="upperSideTop">
+            <img src={gptLogo} alt="" className="logo" />
+            <span className="brand">ChatGPT</span>
+            <button className="midbtn">
+              <img src="" alt="" className="addbtn" />
+              New Chat
+            </button>
+            <div className="upperSideBottom">
+              <button className="query">
+                <img src="" alt="" />
+                What Is Programming ?
+              </button>
+              <button className="query">
+                <img src="" alt="" />
+                What Is Programming ?
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="lowerSide"></div>
       </div>
+      <div className="main"></div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
