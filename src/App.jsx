1660e16d6b3b4b948dacfbcd499c3cd515b97443
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-02-20 13:04:05 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className="sidebar">
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
